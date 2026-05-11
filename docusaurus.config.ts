ccf35401266b9f298b81d47c8e9bb8be2dbaafac
Gastlt <diegomacielpliego@gmail.com>
2026-05-11 12:47:16 -0600

    added github workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://Gastlt.github.io',
+  url: 'https://gastlt.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/DocTest/',
