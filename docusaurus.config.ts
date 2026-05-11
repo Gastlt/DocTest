672f0372a1c57cb2f350dc38abecba4d222fe9a5
Gastlt <diegomacielpliego@gmail.com>
2026-05-11 12:34:00 -0600

    5

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://Gastlt.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/DocTest',
+  baseUrl: '/DocTest/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
